9692e6a8e748a1a780291f1e7fb3bcc1eff9afb7
KKostika <kostikakyriaki1@gmail.com>
2026-03-08 16:49:59 +0200

    Control Flow homework

---
+++ ai_home_projects/py_control_flow/enum.py
@@ -0,0 +1,4 @@
+fav_foods = ["Seafood", "Pizza", "Sushi", "Tacos", "Pasta", "Ice Cream",  "Rissoto", "Burgers", "Salads", "Steak"]
+
+for rank, food in enumerate(fav_foods, start=1):
+    print(f"{rank}. {food}")
---
+++ ai_home_projects/py_control_flow/while.py
@@ -0,0 +1,5 @@
+print("This loop will run until you type quit, ready...\n")
+while True:
+    user_input = input("What is your favorite...of anything?\n")
+    if user_input.lower() == "quit":
+        break
